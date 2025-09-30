c4e3759a1d61689d7135d336ad0d5f24ad63bed2
thanif2000 <taiba.hanif2000@gmail.com>
2025-09-29 20:35:54 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         String name = "Taiba";
-        System.out.println("Hello " + name);
+        System.out.println("Hi " + name);
     }
 }
